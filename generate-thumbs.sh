d5b23808394e8dad60084350187accf8e7ee466c
Chris Smith <chris@dmdirc.com>
2016-03-13 06:58:44 +0000

    Generate thumbnails.

--- generate-thumbs.sh
+++ generate-thumbs.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for file in `find photos -type f | grep -v .thumb. | grep -v .html`; do
-    out="${file%.*}.thumb.${file##*.}";
-    test -e $out || convert -thumbnail 400^ -gravity center -crop 400x200+0+0 $file $out;
+SOURCE=~/Dropbox/photos-site
+
+for folder in $SOURCE/*; do
+    foldername=${folder##*/}
+    mkdir -p photos/$foldername
+
+    for file in $folder/*; do
+        filename=${file##*/}
+        out="photos/$foldername/$filename";
+        test -e $out || convert -thumbnail 400^ -gravity center -crop 400x200+0+0 -strip -quality 86 $file $out;
+    done
 done
 
