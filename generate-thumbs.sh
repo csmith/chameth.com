7274685ee0f02fb37645d8ddd4327dbc092a09e8
Chris Smith <chris@dmdirc.com>
2016-03-13 05:45:59 +0000

    Crop thumbnails.

--- generate-thumbs.sh
+++ generate-thumbs.sh
@@ -2,6 +2,6 @@
 
 for file in `find photos -type f | grep -v .thumb. | grep -v .html`; do
     out="${file%.*}.thumb.${file##*.}";
-    test -e $out || convert -thumbnail 400 $file $out;
+    test -e $out || convert -thumbnail 400^ -gravity center -crop 400x200+0+0 $file $out;
 done
 
