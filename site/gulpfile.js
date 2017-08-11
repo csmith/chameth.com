b69e993643d5681dfceaf3e7b67f9d5fe7d4bee7
Chris Smith <chris@chameth.com>
2017-08-11 20:47:40 +0100

    Remove old build files.

--- site/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-
-gulp.task('hugo:prod', function(callback) {
-    gulp.src('').pipe(plugins.shell(['hugo -v -b https://www.chameth.com/ -d /var/www/html/'], { cwd: process.cwd() })).on('end', callback || function() {});;
-});
-
-gulp.task('default', ['hugo:prod']);
