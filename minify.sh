63f4b8cf55fc29cf83ac6083ebb733988a7617d5
Chris Smith <chris@chameth.com>
2019-10-31 01:32:42 +0000

    Rewrite minify.sh to use find more sensibly

--- minify.sh
+++ minify.sh
@@ -2,13 +2,8 @@
 
 set -uxeo pipefail
 
-# Run tidy over all HTML 
-for file in $(find /tmp/site/ -name '*.html'); do
-	# Tidy exits if there are warnings, which there probably will be...
-	tidy -q -i -w 120 -m --vertical-space yes --drop-empty-elements no "$file" || true
-done
+# Run tidy over all HTML
+find /tmp/site/ -name '*.html' -print -execdir tidy -q -i -w 120 -m --vertical-space yes --drop-empty-elements no "{}" \;
 
 # Convert all images to WebP
-for file in $(find /tmp/site -name '*.jpg' -o -name '*.png' -o -name '*.jpeg'); do
-	cwebp -m 6 -mt -o "$file.webp" -- "$file"
-done
+find /tmp/site \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) -execdir cwebp -m 6 -mt -o "{}.webp" -- "{}" \;
