8d989b9b4fdc6c7a1db893d0a9eafa5079ec9024
Chris Smith <chris@chameth.com>
2024-12-14 17:33:55 +0000

    Add microformat stuff for posts

--- _config/filters/_apply.js
+++ _config/filters/_apply.js
@@ -1,8 +1,10 @@
+import isoDateFormat from "./isodateformat.js";
 import dateFormat from "./dateformat.js";
 import excerpt from "./excerpt.js";
 import head from "./head.js";
 
 export default function (eleventyConfig) {
+    eleventyConfig.addFilter("isoDateFormat", isoDateFormat);
     eleventyConfig.addFilter("dateFormat", dateFormat);
     eleventyConfig.addFilter("excerpt", excerpt);
     eleventyConfig.addFilter("head", head);
