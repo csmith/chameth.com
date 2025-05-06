81418aac0da550ef842ae75928bd1c3c8f269692
Chris Smith <chris@chameth.com>
2025-05-06 21:42:26 +0100

    Interests + screenshot script fiddling

--- screenshot.sh
+++ screenshot.sh
@@ -4,30 +4,26 @@ set -eux
 
 # To avoid interfering with the host's node_modules, copy everything
 # to a temporary dir and then blat the node stuff.
-SITE_COPY=$(mktemp -d)
+SITE_COPY=$(mktemp -d --tmpdir chameth.com-screenshot.XXXXXXXX)
 cp -a . "$SITE_COPY"
 rm -rf "$SITE_COPY/node_modules"
 
-# First build the site as the current user, and fiddle with the
-# permissions to allow global write access (yuck). There's no reason
-# for this to use puppeteer, but it saves pulling a different image.
+# Build the site 
 docker run --rm \
-  -v "$SITE_COPY:/site" \
+  -v "$SITE_COPY:/site:U" \
   --entrypoint /bin/sh \
-  --user "$(id -u)" \
   ghcr.io/puppeteer/puppeteer:latest \
   -c '
     set -eux
     cd /site
     npm ci
     npm run build
-    chmod -R o+w /site
     '
 
 # Run puppeteer to take the screenshot. This runs as the puppeteer user
 # as that's where the puppeteer stuff pre-installed.
 docker run --rm \
-  -v "$SITE_COPY:/site" \
+  -v "$SITE_COPY:/site:U" \
   -i \
   --cap-add=SYS_ADMIN \
   --init \
@@ -36,6 +32,3 @@ docker run --rm \
 
 # Resize the screenshot down and move it into the real location.
 magick "$SITE_COPY/static/screenshot.png" -scale 640x360 "./static/screenshot.png"
-
-# Clean up.
-rm -rf "$SITE_COPY"
