1a91c3f3dbb613c27065d90a85ad1b39f3f69fc9
Chris Smith <chris@chameth.com>
2025-05-22 20:45:40 +0100

    Fail the build if I there's no <!--more-->

--- _config/filters/excerpt.js
+++ _config/filters/excerpt.js
@@ -2,7 +2,11 @@ import md from "../libraries/markdown.js";
 import liquid from "../libraries/liquid.js";
 
 export default async (page) => {
-    return md.render(
+    if (!page.excerpt && page.inputPath && page.inputPath.includes('/posts/')) {
+        throw new Error(`Missing <!--more--> in post "${page.inputPath}"`);
+    }
+    
+    return page.excerpt && md.render(
         await liquid.parseAndRender(page.excerpt, {page})
-    ).replaceAll(/\[\^\d+]/g, '');
+    ).replaceAll(/\[\^\d+]/g, '') || '';
 }
--- _config/filters/summary.js
+++ _config/filters/summary.js
@@ -2,6 +2,6 @@ import excerpt from "./excerpt.js";
 
 export default async (page) => {
     return /<p>([\s\S]*?)<\/p>/
-        .exec(await excerpt(page))[1]
-        .replaceAll(/<.*?>/g, '');
+        .exec(await excerpt(page))?.[1]
+        .replaceAll(/<.*?>/g, '') || '';
 }
